95541344e433d838289e9f092ad4a6cc16029a9c
Pedro Segato <pedrosegato4@gmail.com>
2024-01-29 20:55:24 -0400

    fix: new openGraph image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,30 +5,44 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "SeuCampo - Reserve seu campo sem dor de cabeça online!",
-  description:
-    "Transforme a gestão do seu campo de futebol com facilidade e eficiência! Agora, você pode reservar campos instantaneamente e impulsionar seus lucros enquanto elimina as dores de cabeça do agendamento manual. Faça parte dessa revolução através de uma plataforma intuitiva que simplifica a vida tanto dos proprietários quanto dos clientes!",
-  creator: "SeuCampo",
-  keywords:
-    "futebol, futsal, quadra, campo, jogar, alugar, campeonato, brasileiro, agendamento de campos, futebol, reserva de campos, dono de campo",
-  openGraph: {
-    title: "SeuCampo - Reserve seu campo sem dor de cabeça online!",
-    description:
-      "Transforme a gestão do seu campo de futebol com facilidade e eficiência! Agora, você pode reservar campos instantaneamente e impulsionar seus lucros enquanto elimina as dores de cabeça do agendamento manual. Faça parte dessa revolução através de uma plataforma intuitiva que simplifica a vida tanto dos proprietários quanto dos clientes!",
-    url: "https://seucampo.com.br",
-  },
-  robots: "index, follow",
+	title: "SeuCampo - Reserve seu campo sem dor de cabeça online!",
+	description:
+		"Transforme a gestão do seu campo de futebol com facilidade e eficiência! Agora, você pode reservar campos instantaneamente e impulsionar seus lucros enquanto elimina as dores de cabeça do agendamento manual. Faça parte dessa revolução através de uma plataforma intuitiva que simplifica a vida tanto dos proprietários quanto dos clientes!",
+	creator: "SeuCampo",
+	keywords:
+		"seu campo, futebol, futsal, quadra, campo, jogar, alugar, campeonato, brasileiro, agendamento de campos, futebol, reserva de campos, dono de campo",
+	openGraph: {
+		title: "SeuCampo - Reserve seu campo sem dor de cabeça online!",
+		description:
+			"Transforme a gestão do seu campo de futebol com facilidade e eficiência! Agora, você pode reservar campos instantaneamente e impulsionar seus lucros enquanto elimina as dores de cabeça do agendamento manual. Faça parte dessa revolução através de uma plataforma intuitiva que simplifica a vida tanto dos proprietários quanto dos clientes!",
+		url: "https://seucampo.com.br",
+		images: [
+			{
+				url: "/public/SeuCampoFull.png",
+				alt: "Logo SeuCampo",
+				width: 800,
+				height: 600,
+			},
+		],
+		locale: "pt_BR",
+		type: "website",
+	},
+	robots: "index, follow",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
-      <link rel="icon" type="image/png" href="SeuCampoIcon.png" />
-    </html>
-  );
+	return (
+		<html lang="pt-br">
+			<body className={inter.className}>{children}</body>
+			<link
+				rel="icon"
+				type="image/png"
+				href="SeuCampoIcon.png"
+			/>
+		</html>
+	);
 }
