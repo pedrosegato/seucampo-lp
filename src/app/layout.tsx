060fec01d9cf722e31efd0fce5929211bd1c35c7
MarcosGaius <gaiusmarcos@gmail.com>
2024-02-01 19:47:17 -0400

    fix: fixing misplaced scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,36 +38,39 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <Script
-        id="gtm"
-        dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      <head>
+        <Script
+          id="gtm"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 					new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 					j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 					'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-					})(window,document,'script','dataLayer','GTM-MJRR7RQL');`,
-        }}
-      />
-
-      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-D0940ZQMKT"></Script>
-      <Script
-        id="gta"
-        dangerouslySetInnerHTML={{
-          __html: `window.dataLayer = window.dataLayer || [];
-				function gtag(){dataLayer.push(arguments);}
-				gtag('js', new Date());
-				gtag('config', 'G-D0940ZQMKT');`,
-        }}
-      />
-      <body className={inter.className}>{children}</body>
-      <noscript>
-        <iframe
-          src="https://www.googletagmanager.com/ns.html?id=GTM-MJRR7RQL"
-          height="0"
-          width="0"
-          style={{ display: "none", visibility: "hidden" }}
-        ></iframe>
-      </noscript>
+				})(window,document,'script','dataLayer','GTM-MJRR7RQL');`,
+          }}
+        />
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-D0940ZQMKT" />
+        <Script
+          id="gta"
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+					function gtag(){dataLayer.push(arguments);}
+					gtag('js', new Date());
+					gtag('config', 'G-D0940ZQMKT');`,
+          }}
+        />
+      </head>
+      <body className={inter.className}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MJRR7RQL"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {children}
+      </body>
       <link rel="icon" type="image/png" href="SeuCampoIcon.png" />
     </html>
   );
